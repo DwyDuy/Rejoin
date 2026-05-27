6bd8a3678f8829a3f2240dea4e9bb5906a65e489
DwyDuy <hadcl001@gmail.com>
2026-05-27 09:33:05 +0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+# Tự động tải code về thư mục chính
+rm -rf ~/RobloxAuto
+mkdir -p ~/RobloxAuto
+curl -L https://raw.githubusercontent.com/TEN_USER_CUA_BAN/RobloxAuto/main/rejoin.py -o ~/RobloxAuto/rejoin.py
+echo "Đã cài đặt xong!"
